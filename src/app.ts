3b799dd4b5cb6c18a658c44dfd3fb71077bb33ff
Sairam Cr <crsairam7617@gmail.com>
2024-06-19 14:11:59 +0530

    removed hello

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-  
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
